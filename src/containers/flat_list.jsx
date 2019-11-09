8f15643bab49f87bcef80c4422d3634a67837baa
Anders <kiss7medical@gmail.com>
2019-11-09 12:13:57 -0500

    add scss and defaultProps to flat_list container

--- src/containers/flat_list.jsx
+++ src/containers/flat_list.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
 class FlatList extends Component {
+	
+	// TEMP CODE TO INTEGRATE HTML
+	static defaultProps = {
+	  flats: [{
+		"name": Charmy Loft,
+		"imageURL": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
+		"price": 123,
+		"priceCurrency": "USD"
+		}]
+	}
+
 	render() {
 		return (
-			<div className="col-sm-7">
-				TODO		
+			<div className="flat-list col-sm-7">
+				{this.props.flats.map((flat) => <Flat flat={flat} key={flat.name}/>)}		
 			</div>
 		);
 	}
